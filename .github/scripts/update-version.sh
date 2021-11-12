260e34e0fa4abae5fdb21b3e1df7ad3ba9099213
Telan <13066272+TheElan@users.noreply.github.com>
2021-11-12 19:48:51 +0200

    fix update-version.sh checking wrong file

--- .github/scripts/update-version.sh
+++ .github/scripts/update-version.sh
@@ -1,10 +1,10 @@
-if [ ! -z $(git diff --name-only HEAD HEAD~1 | grep dependencies.gradle) ]; then
+if [ ! -z $(git diff --name-only HEAD HEAD~1 | grep build.gradle) ]; then
   new_version="$(git log -n1 --format=format:"%H")"
   sed --in-place "s!//version:.*!//version: $new_version!g" build.gradle
   git add build.gradle
-  git commit -m "[ci skip] update buildscript version to $new_version"
+  git commit -m "[ci skip] update build script version to $new_version"
   git push
-  echo "updated buildscript version to $new_version"; 
+  echo "Updated build script version to $new_version"; 
 else
-  echo "no buildscript changes detected - ignoring version update"
+  echo "Ignored build script version update: no changed detected"
 fi
