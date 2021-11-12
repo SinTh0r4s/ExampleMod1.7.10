82c8c9dcad3465ef46c4d0518c447a42388244f4
Telan <13066272+TheElan@users.noreply.github.com>
2021-11-12 20:12:37 +0200

    fix update-version.sh not pushing HEAD to branch

--- .github/scripts/update-version.sh
+++ .github/scripts/update-version.sh
@@ -1,9 +1,10 @@
+git checkout ${GITHUB_REF##*/}
 if [ ! -z $(git diff --name-only HEAD HEAD~1 | grep build.gradle) ]; then
   new_version="$(git log -n1 --format=format:"%H")"
   sed --in-place "s!//version:.*!//version: $new_version!g" build.gradle
   git add build.gradle
   git commit -m "[ci skip] update build script version to $new_version"
-  git push origin HEAD:${GITHUB_REF##*/}
+  git push
   echo "Updated buildscript version to $new_version"; 
 else
   echo "Ignored buildscript version update: no changes detected"
