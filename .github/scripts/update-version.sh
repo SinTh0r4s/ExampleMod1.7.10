ef157d88076651d2b0e2b7c13f6d5d4f93933fcd
Telan <13066272+TheElan@users.noreply.github.com>
2021-11-12 19:51:49 +0200

    ci: configure git before committing updated buildscript

--- .github/scripts/update-version.sh
+++ .github/scripts/update-version.sh
@@ -1,10 +1,14 @@
 if [ ! -z $(git diff --name-only HEAD HEAD~1 | grep build.gradle) ]; then
   new_version="$(git log -n1 --format=format:"%H")"
   sed --in-place "s!//version:.*!//version: $new_version!g" build.gradle
+  
+  git config user.email "johann.bernhardt@tum.de"
+  git config user.name "Github CI Worker"
+  
   git add build.gradle
   git commit -m "[ci skip] update build script version to $new_version"
   git push
-  echo "Updated build script version to $new_version"; 
+  echo "Updated buildscript version to $new_version"; 
 else
-  echo "Ignored build script version update: no changed detected"
+  echo "Ignored buildscript version update: no changes detected"
 fi
