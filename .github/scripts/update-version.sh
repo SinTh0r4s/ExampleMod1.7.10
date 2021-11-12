01c33c5044089be6b8b7b76edf925474c7c24648
Telan <13066272+TheElan@users.noreply.github.com>
2021-11-12 19:27:28 +0200

    add message when ignoring version update

--- .github/scripts/update-version.sh
+++ .github/scripts/update-version.sh
@@ -2,7 +2,9 @@ if [ ! -z $(git diff --name-only HEAD HEAD~1 | grep dependencies.gradle) ]; then
   new_version="$(git log -n1 --format=format:"%H")"
   sed --in-place "s!//version:.*!//version: $new_version!g" build.gradle
   git add build.gradle
-  git commit -m "update buildscript version to $new_version"
+  git commit -m "[ci skip] update buildscript version to $new_version"
   git push
-  echo "[ci skip] update buildscript version"; 
+  echo "updated buildscript version to $new_version"; 
+else
+  echo "no buildscript changes detected - ignoring version update"
 fi
