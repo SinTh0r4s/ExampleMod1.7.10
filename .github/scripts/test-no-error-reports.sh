e8824913850ef6b578ba73e35478691f546bd820
Dmytro <13066272+TheElan@users.noreply.github.com>
2021-12-13 16:08:07 +0200

    add startup finish check

--- .github/scripts/test-no-error-reports.sh
+++ .github/scripts/test-no-error-reports.sh
@@ -16,6 +16,12 @@ if grep --quiet "The state engine was in incorrect state ERRORED and forced into
   exit 1
 fi
 
+if grep --quiet 'Done .+ For help, type "help" or "?"' server.log; then
+  echo "Server didn't finish startup:"
+  cat server.log
+  exit 1
+fi
+
 echo "No crash reports detected"
 exit 0
 
